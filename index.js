a40c9d077afa5f5bae22ffb3ecab5f0886cf60b1
duklfuc <tranducphuc20072003@gmail.com>
2024-01-31 02:20:55 +0700

    Install Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const route = require("./routes/client/index.route");
+
+const database = require("./config/database")
 require('dotenv').config();
 
-
+database.connetc();
 
 const app = express();
 const port = process.env.PORT;
